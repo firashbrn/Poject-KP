417fb761d69cce3cde051201fbbf6c9f893130b9
Magfira Shabrina <magfirashabrina@gmail.com>
2026-02-03 13:04:08 +0700

    peubahan 1

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -2,7 +2,7 @@ class ApiConstants {
   // Base URL
   static const String baseUrl = String.fromEnvironment(
     'API_BASE_URL',
-    defaultValue: 'http://10.0.2.2:3000/apim',
+    defaultValue: 'http://10.0.2.2:3000/api',
   );
 
   //ENDPOINTS
